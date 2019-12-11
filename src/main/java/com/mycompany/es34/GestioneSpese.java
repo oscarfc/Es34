10a02eb1465056b5deb2b9cd4d084d47a3e1ab51
tss <tss@debian>
2019-12-11 15:54:00 +0100

    aggiunto parte mancante e corretto errori

--- src/main/java/com/mycompany/es34/GestioneSpese.java
+++ src/main/java/com/mycompany/es34/GestioneSpese.java
@@ -12,6 +12,7 @@ import javax.swing.JOptionPane;
  * @author tss
  */
 public class GestioneSpese {
+
     private Spese archivioSpese;
 
     public static void main(String[] args) {
@@ -19,35 +20,39 @@ public class GestioneSpese {
 
         int numMaxSpese = gSpese.AskInt("Inserisci numero spese che vuoi gestire", 1, 100);
         gSpese.gestisciInsertSpese(numMaxSpese);
-        
+
         JOptionPane.showMessageDialog(null, gSpese.stampaSpese(numMaxSpese));
         JOptionPane.showMessageDialog(null, gSpese.stampaSpeseConTotale(numMaxSpese));
+
+        // parte 2 di es34
+        gSpese.gestisciInsertSpeseConAnticipo(numMaxSpese);
+        JOptionPane.showMessageDialog(null, gSpese.stampaSpeseConAnticipo(numMaxSpese));
     }
 
     public void gestisciInsertSpese(int numeroSpese) {
         archivioSpese = new Spese(numeroSpese);
         for (int i = 0; i < numeroSpese; i++) {
-            archivioSpese.inserisciSpesa(AskSpesa("Inserisci " + (i + 1) + " spesa", -1, 1000f));
+            archivioSpese.inserisciSpesa(AskSpesa("Inserisci " + (i + 1) + " spesa", 1, 1000f));
         }
     }
-    
+
     public String stampaSpese(int numeroSpese) {
         String stampa = "Elenco Spese:\n";
         for (int i = 0; i < numeroSpese; i++) {
             stampa += (i + 1) + " -  " + archivioSpese.getValoreSpesa(i) + "\n";
         }
-        
+
         return stampa;
     }
-    
-   public String stampaSpeseConTotale(int numeroSpese) {
+
+    public String stampaSpeseConTotale(int numeroSpese) {
         String stampa = "Elenco Spese:\n";
         float totale = 0;
-        
+
         for (int i = 0; i < numeroSpese; i++) {
-            totale += archivioSpese.getValoreSpesa(i);
             stampa += (i + 1) + " -  " + archivioSpese.getValoreSpesa(i) + "\n";
         }
+        totale = archivioSpese.getTotale();
         stampa += "Totale Spesa: " + totale + "\n";
         stampa += "Spesa media: " + totale / numeroSpese + "\n";
         return stampa;
@@ -59,7 +64,7 @@ public class GestioneSpese {
         boolean controllo = false;
 
         while (!controllo) {
-            strInt = JOptionPane.showInputDialog(testo + " - minimo" + min + "e massimo" + max);
+            strInt = JOptionPane.showInputDialog(testo + " - minimo " + min + " e massimo " + max);
             try {
                 ret = Integer.parseInt(strInt);
                 if (ret > min && ret <= max) {
@@ -80,7 +85,7 @@ public class GestioneSpese {
         boolean controllo = false;
 
         while (!controllo) {
-            strFloat = JOptionPane.showInputDialog(testo + " - minimo" + min + "e massimo" + max);
+            strFloat = JOptionPane.showInputDialog(testo + " - minimo " + min + " e massimo " + max);
             try {
                 ret = Float.parseFloat(strFloat);
                 if (ret > min && ret <= max) {
@@ -95,4 +100,24 @@ public class GestioneSpese {
         return ret;
     }
 
+    private void gestisciInsertSpeseConAnticipo(int numMaxSpese) {
+        archivioSpese.setAnticipo(AskSpesa("Inserisci anticipo", 1, 1000f));
+    }
+
+    private String stampaSpeseConAnticipo(int numeroSpese) {
+        String stampa = stampaSpeseConTotale(numeroSpese);
+        stampa += "Anticipo: " + archivioSpese.getAnticipo() + "\n";
+        stampa += getDifferenzaAnticipo() + "\n";
+        return stampa;
+    }
+
+    private String getDifferenzaAnticipo() {
+        String ret = "";
+        if (archivioSpese.getTotale() > archivioSpese.getAnticipo()) {
+            ret += "Devo avere: " + (archivioSpese.getTotale() - archivioSpese.getAnticipo());
+        } else {
+           ret += "Avanzati: " + ((archivioSpese.getTotale() - archivioSpese.getAnticipo()) * -1);
+        }
+        return ret;
+    }
 }
--- src/main/java/com/mycompany/es34/Spese.java
+++ src/main/java/com/mycompany/es34/Spese.java
@@ -10,7 +10,9 @@ package com.mycompany.es34;
  * @author tss
  */
 public class Spese {
-    private float[] spese; 
+
+    private final float[] spese;
+    private float anticipo;
     private int next = 0;
 
     public Spese(int numMaxspese) {
@@ -24,5 +26,25 @@ public class Spese {
     float getValoreSpesa(int i) {
         return spese[i];
     }
-    
+
+    public float getAnticipo() {
+        return anticipo;
+    }
+
+    public float getTotale() {
+        float totale = 0;
+        for (int i = 0; i < next; i++) {
+            totale += getValoreSpesa(i);
+        }
+        return totale;
+    }
+
+    public void setAnticipo(float anticipo) {
+        this.anticipo = anticipo;
+    }
+
+    @Override
+    public String toString() {
+        return spese.toString();
+    }
 }
